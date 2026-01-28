b2c03e98066dbb64e03ac67eb6a3410f819ce994
phoom0310-sketch <phoom0310@gmail.com>
2026-01-28 11:23:46 +0700

    Add delete_user.php for user deletion functionality

---
+++ ajax/delete_user.php
@@ -0,0 +1,10 @@
+<?php
+require '../db.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+$id = (int)$data['id'];
+
+$stmt = $pdo->prepare("DELETE FROM users WHERE id=?");
+$stmt->execute([$id]);
+
+echo json_encode(['success'=>true]);
